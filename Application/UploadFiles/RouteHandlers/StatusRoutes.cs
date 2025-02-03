585673336e592b970bea78ac9a7d0b2661567d79
EDU-COMPUTER\Eduardo <eduardosantamarco123@outlook.com>
2025-02-03 00:42:03 -0300

    Merge branch 'develop'
    # Conflicts:
    #	AOT/UploadFiles/Program.cs
    #	AOT/UploadFiles/UploadFiles.csproj
    #	AOT/UploadFiles/appsettings.json
    #	Services/UploadFiles.Services/Services/Upload/UploadManager.cs
    #	UploadFiles.sln
    #	docker-compose.yml

---
+++ Application/UploadFiles/RouteHandlers/StatusRoutes.cs
@@ -0,0 +1,16 @@
+﻿namespace UploadFiles.RouteHandlers
+{
+    public static class StatusRoutes
+    {
+        public static void Configure(RouteGroupBuilder statusMap)
+        {
+            //statusMap.MapGet("/{referenceId}", GetStatus);
+        }
+
+        private static object? GetStatusFromReferenceId(string referenceId)
+        {
+            // Implementation of status retrieval logic
+            throw new NotImplementedException();
+        }
+    }
+}
