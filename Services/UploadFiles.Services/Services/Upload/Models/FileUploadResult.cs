6da5166f4e386589d9bbbf9661b41b57cd8dfbf2
EDU-COMPUTER\Eduardo <eduardosantamarco123@outlook.com>
2025-02-14 00:24:01 -0300

    remove files

--- Services/UploadFiles.Services/Services/Upload/Models/FileUploadResult.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace UploadFiles.Services.Services.Upload.Models
-{
-    public record FileUploadResult(bool SuccessIdentified, bool SuccessSentToProccess, string FileName, 
-                                string FileType, string FileExtension,
-                                DateTime dateSent)
-    {     
-    }
-}
